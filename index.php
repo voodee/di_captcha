6a14863d36b213e18b4afd53c4e86d942e077bf0
voodee <kidala@xaker.ru>
2012-11-23 00:54:47 +0400

    up

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+  // index.php
+  // Прример использования класса
+  session_start();
+  // THE_NUMBER_OF_LETTERS – константа, количество символов в строке
+  define('THE_NUMBER_OF_LETTERS', 6);
+  // При запросе от js о генерации строки, возвращается количество символов и сам массив координат.
+  if (isset($_POST['action']{14}) && $_POST['action'] == 'captcha_refresh') {
+    require 'phar://di_captcha.class.phar.gz/di_captcha.class.php';
+    $captcha = new di\captcha();
+    $captcha->set('noise', 0);
+    echo json_encode(array(THE_NUMBER_OF_LETTERS, $captcha->get()));  } else {
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset='utf-8'>
+  <title>Test</title>
+  <link rel='stylesheet' media='all' href='style.css'>
+  <script type='text/javascript' src='jquery-1.6.1.min.js' charset='utf-8'></script>
+  <script type='text/javascript' src='script.js' charset='utf-8'></script>
+</head>
+<body>
+  <p id='Title'>Сим-сим, откройся!</p>
+  <p id='Msg'>
+  <?php
+    if (isset($_POST['action']{11}) && $_POST['action'] == 'captcha_send') {;
+      require 'phar://di_captcha.class.phar.gz/di_captcha.class.php';
+      $captcha = new di\captcha();
+      echo ($captcha->check($_POST['text_captcha']))?'Сим-сим открылся!':'К сожалению, Вы ошиблись...';
+    }
+  ?>
+  </p>
+  <form action='index.php' method='post'>
+    <div id='DICaptchaPic'></div>
+    <p style='padding: 0 10px;'>
+      <input type='text' name='text_captcha' id='text_captcha' value='<?php echo $_POST['text_captcha']; ?>' placeholder='6 символов с картинки'><br><label for='text_captcha'>*aнти-спам</label> <a href='#' onclick='di_captcha_refresh(); return false;'>Не вижу</a>
+    </p>
+    <p style='padding: 10px 0;'>
+      <input type='hidden' name='action' value='captcha_send' />
+      <input type='submit' name='submit' value='Проверить' />
+    </p>
+  </form>
+</body>
+</html>
+<?php
+  }
+?>
---
+++ script.js
@@ -0,0 +1,23 @@
+/* script.js */
+/* cell_size Ц размер €чеек в пиксел€х */
+var cell_size = 3;
+function di_captcha_refresh() {  $.post('./index.php', {action: 'captcha_refresh'},
+    function(data) {      var data = eval(data);
+      $('#DICaptchaPic').css('width', ((((cell_size+2)*6)+(3*cell_size)+1)*data[0]));
+      var html_p_tag = '';
+      for (i = 1; i <= 7*7*data[0]; ++i)  {
+        var style = (i%7 == 0)?'margin-right: '+2*cell_size+'px;':'';
+        for (j = 0; j < data[1].length; j += 2) style +=(((i%(data[0]*7)==0)?(data[0]*7):i%(data[0]*7)) == data[1][j] && Math.ceil(i/(data[0]*7)) == data[1][j+1])?'background-color: #000;':'';
+        html_p_tag += '<p'+((style=='')?'':' style=\''+style+'\'')+'></p>';
+      }
+      $('#DICaptchaPic').html(html_p_tag);
+    }
+  )
+}
+
+$(document).ready(function() {
+  $('#DICaptchaPic').css('overflow', 'hidden');
+  $('#DICaptchaPic').css('height', (cell_size+2)*7);  di_captcha_refresh();
+  $('#DICaptchaPic').click(function() {di_captcha_refresh();});
+  $('#text_captcha').focus()
+})
